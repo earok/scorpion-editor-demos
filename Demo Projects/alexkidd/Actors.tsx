e50cde56a775039c9361dd92976733ca3cdc9a30
Erik <earok3d@gmail.com>
2019-11-10 12:23:53 +1300

    Initial check in

---
+++ Demo Projects/alexkidd/Actors.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="name" value="alexkidd" type="string" />
+      <property name="uid" value="8ad7116c-d75a-4b9e-8fcc-cbd612da11a5" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="name" value="alexkidd_ghost" type="string" />
+      <property name="uid" value="80c5369a-2fe2-4346-a6c6-b920b2734bab" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="name" value="alexkidd_swim" type="string" />
+      <property name="uid" value="f40e1522-aeaf-4d26-ad73-6402c9ec9995" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="name" value="blockbreakleft" type="string" />
+      <property name="uid" value="a65919ca-2e8a-4d16-a30d-700525f9e11a" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="name" value="blockbreakright" type="string" />
+      <property name="uid" value="ff958c13-5966-4c4e-a7ed-80ff0c69ef53" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="name" value="killpuff" type="string" />
+      <property name="uid" value="e0f3eaff-82df-42f6-b311-963cfeef8456" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="name" value="ringshot" type="string" />
+      <property name="uid" value="dfccd319-412b-4114-aaa8-b5a712e43324" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="name" value="smallfish" type="string" />
+      <property name="uid" value="1e6e0853-71c6-49cf-a034-5ac8943b8d68" type="string" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="name" value="bird" type="string" />
+      <property name="uid" value="4597993b-2c10-4324-aa99-29abeef5a0bc" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="name" value="bigfish" type="string" />
+      <property name="uid" value="d5c55ab1-bda7-458f-8204-bf6b71b73092" type="string" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="name" value="merman" type="string" />
+      <property name="uid" value="d9fd3cf8-6706-4ca0-a125-e2bcaef69668" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ Demo Projects/alexkidd/project.ink
@@ -0,0 +1,125 @@
+CONST Song_Start = 0
+CONST Song_StageComplete = 16
+CONST Song_Water = 17
+
+CONST CheckpointNA = 0
+CONST CheckpointLand = 1
+CONST CheckpointWater = 2
+VAR CheckpointType = 0
+VAR CheckpointX = 0
+VAR CheckpointY = 0
+VAR Ammo = 0
+~ yres = 192
+
+=== Startup ===
+~ checkpointtype = CheckpointNA
+~ panel=pnl_title
+~ wait=0
+~ panel=null
+
+=== RestartLevel ===
+~ music = song
+~ musicpos = song_start
+~ ammo = 0
+~ camera_follow = camera_follow_down
+~ level = level1
+
+{ CheckpointType:
+	~ player_X = CheckpointX
+	~ player_Y = CheckpointY
+	{ CheckpointType == CheckpointWater:
+		-> HitWater
+	}
+}
+-> GAME
+
+=== entered_checkpoint ===
+{ player_Type == AlexKidd:
+	~ CheckpointType = CheckpointLand
+}
+{ player_Type == AlexKidd_Swim:
+	~ CheckpointType = CheckpointWater
+}
+~ CheckpointX = Block_X * 16 + 8
+~ CheckpointY = Block_Y * 16 + 8
+~ Block_Type = null
+-> GAME
+
+=== HitWater ===
+~ musicpos = song_water
+~ sound = sndwater
+{ player_Type == alexkidd :
+	~player_Type = alexkidd_swim
+}
+~camera_follow = camera_follow_down + camera_follow_right
+-> GAME
+
+=== alex_punch ===
+{ ammo:
+	~ player_shoot = ringshot
+	~ ammo = ammo - 1
+}
+->GAME
+
+=== player_kill ===
+~music = null
+~sound = snddie
+~player_type = alexkidd_ghost
+~yield = 100
+-> RestartLevel
+
+=== SpawnBrokenBlock ===
+~ sound = sndblock
+~ block_spawn = blockbreakleft
+~ block_spawn = blockbreakright
+->->
+
+=== BreakBlock ===
+-> SpawnBrokenBlock ->
+{ block_type == MysteryContainer :
+	~ block_type = Ring
+	-> GAME
+}
+{ block_type == StarContainer :
+	~ block_type = Money
+	-> GAME
+}
+{ block_type == UnderwaterBurgerRock:
+	~ block_type = burger
+	-> GAME
+}
+~ block_type = null
+-> GAME
+
+=== GetMoney ===
+~ sound = sndcoin
+~ block_type = null
+-> GAME
+
+=== GetRing ===
+~ sound = sndcoin
+~ ammo = 10
+~ block_type = null
+-> GAME
+
+=== Kill_Enemy ===
+~ sound = sndenemykill
+~ actor_type = killpuff
+-> GAME
+
+=== merman_spawn ===
+~ actor_var1 = 5
+-> GAME
+
+=== hit_merman ===
+~ actor_var1 = actor_var1 - 1
+{ actor_var1 < 1:
+	-> Kill_Enemy	
+}
+-> GAME
+
+=== finishlevel ===
+~ musicpos = song_stagecomplete
+~ level = null
+-> StartUp
+
