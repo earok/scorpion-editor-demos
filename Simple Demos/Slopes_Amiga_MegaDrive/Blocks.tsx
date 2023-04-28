9a862a0e86eb557813e07879f03032d55e210ebd
Erik Hogan <earok3d@gmail.com>
2023-04-28 17:44:38 +1200

    Soft launch 2023.1

---
+++ Simple Demos/Slopes_Amiga_MegaDrive/Blocks.tsx
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+  <tile id="0" />
+  <tile id="1" />
+  <tile id="2" />
+  <tile id="3" />
+  <tile id="4" />
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12">
+    <properties>
+      <property name="Name" value="slope1" type="string" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="Name" value="slope2" type="string" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="Name" value="slope3" type="string" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="Name" value="slope4" type="string" />
+    </properties>
+  </tile>
+  <tile id="16">
+    <properties>
+      <property name="Name" value="slope5" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="slope6" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="slope7" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="slope8" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="slope9" type="string" />
+    </properties>
+  </tile>
+  <tile id="21">
+    <properties>
+      <property name="Name" value="slope10" type="string" />
+    </properties>
+  </tile>
+  <tile id="22">
+    <properties>
+      <property name="Name" value="slope11" type="string" />
+    </properties>
+  </tile>
+  <tile id="23">
+    <properties>
+      <property name="Name" value="slope12" type="string" />
+    </properties>
+  </tile>
+  <tile id="24">
+    <properties>
+      <property name="Name" value="slope13" type="string" />
+    </properties>
+  </tile>
+  <tile id="25">
+    <properties>
+      <property name="Name" value="slope14" type="string" />
+    </properties>
+  </tile>
+  <tile id="26">
+    <properties>
+      <property name="Name" value="slope15" type="string" />
+    </properties>
+  </tile>
+  <tile id="27">
+    <properties>
+      <property name="Name" value="slope_l1" type="string" />
+    </properties>
+  </tile>
+  <tile id="28">
+    <properties>
+      <property name="Name" value="slope_l2" type="string" />
+    </properties>
+  </tile>
+  <tile id="29">
+    <properties>
+      <property name="Name" value="slope_l3" type="string" />
+    </properties>
+  </tile>
+  <tile id="30">
+    <properties>
+      <property name="Name" value="slope_l4" type="string" />
+    </properties>
+  </tile>
+  <tile id="31">
+    <properties>
+      <property name="Name" value="slope_r1" type="string" />
+    </properties>
+  </tile>
+  <tile id="32">
+    <properties>
+      <property name="Name" value="slope_r2" type="string" />
+    </properties>
+  </tile>
+  <tile id="33">
+    <properties>
+      <property name="Name" value="slope_r3" type="string" />
+    </properties>
+  </tile>
+  <tile id="34">
+    <properties>
+      <property name="Name" value="slope_r4" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ Simple Demos/Translucency_Amiga/Blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+</tileset>
